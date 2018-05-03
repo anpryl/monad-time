26c62556ce658b05e943c2d64589b2c70f076e42
Anatolii Prylutskyi <anpryl@gmail.com>
2018-05-03 10:36:07 +0300

    Remove OVERLAPPING from ReaderT instance

--- src/Control/Monad/Time.hs
+++ src/Control/Monad/Time.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE CPP, FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE CPP                  #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 #if __GLASGOW_HASKELL__ < 710
 {-# LANGUAGE OverlappingInstances #-}
@@ -6,9 +9,9 @@
 
 module Control.Monad.Time (MonadTime(..)) where
 
-import Control.Monad.Trans
-import Control.Monad.Reader (ReaderT, ask)
-import Data.Time
+import           Control.Monad.Reader (ReaderT, ask)
+import           Control.Monad.Trans
+import           Data.Time
 
 -- | Class of monads which carry the notion of the current time.
 class Monad m => MonadTime m where
@@ -22,7 +25,7 @@ instance MonadTime IO where
 -- downstream.
 --
 -- @since 0.3.0.0
-instance {-# OVERLAPPING #-} Monad m => MonadTime (ReaderT UTCTime m) where
+instance Monad m => MonadTime (ReaderT UTCTime m) where
   currentTime = ask
 
 -- | Generic, overlapping instance.
